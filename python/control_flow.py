64d28752cc02d257d8b936b67be374f13b708423
User 01 <user01@teaching-F011.(none)>
2017-02-28 15:52:15 +0000

    list count added

--- python/control_flow.py
+++ python/control_flow.py
@@ -2,3 +2,11 @@ while 1 != 1:
     print 'yoooo'
 
 print 'hello'
+
+mylist = [23,"hi",2.4e-10]
+count = 0
+
+while count < 3:
+   item=mylist[count]
+   print item, mylist.index(item)
+   count += 1 
