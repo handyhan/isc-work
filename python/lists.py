5107f95c4963f368f431dbb96a562767bbd22e33
User 01 <user01@teaching-F011.(none)>
2017-03-01 09:30:46 +0000

    list basics

---
+++ python/lists.py
@@ -0,0 +1,16 @@
+mylist = range(1,6)
+
+print mylist[1]
+print mylist[-2]
+
+print mylist[:]
+print mylist[1:4] 
+
+one_to_ten = range(1,11)
+print one_to_ten
+
+one_to_ten.append(11)
+print one_to_ten
+
+one_to_ten.extend([12,13,14])
+print one_to_ten
